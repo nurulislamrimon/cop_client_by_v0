a96cbbcda9760c1645a921db782e4cc046c24175
nurulislamrimon <nurulislamrimon@gmail.com>
2025-04-25 17:50:14 +0600

    [ADD_ROUTE/PROTECTED]

--- middleware.tsx
+++ middleware.tsx
@@ -1,10 +1,7 @@
-// middleware.ts
 import { NextRequest, NextResponse } from "next/server"
-// import { verifyAuthToken } from "./lib/auth" // Custom helper you’ll write
 
 // Define protected routes and roles (if needed)
-const protectedRoutes = ["/finance"]
-const adminRoutes = ["/admin"]
+const protectedRoutes = ["/finance", "/add"]
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
@@ -25,19 +22,16 @@ export async function middleware(req: NextRequest) {
 
       const user = userData && JSON.parse(userData as string)
 
-      // Optional: protect admin-only routes
-      if (user && user?.role !== "super_admin") {
-        return NextResponse.redirect(new URL("/unauthorized", req.url))
+      if (user && user?.role === "super_admin") {
+        return NextResponse.next()
       }
+      // return NextResponse.redirect(new URL("/unauthorized", req.url))
 
-      // You can also attach user info to the request
       const requestHeaders = new Headers(req.headers)
       requestHeaders.set("x-user-id", user.id)
-      return NextResponse.next({ request: { headers: requestHeaders } })
 
-      return NextResponse.next()
+      return NextResponse.next({ request: { headers: requestHeaders } })
     } catch (error) {
-      // Invalid token
       return NextResponse.redirect(new URL("/login", req.url))
     }
   }
@@ -45,7 +39,6 @@ export async function middleware(req: NextRequest) {
   return NextResponse.next()
 }
 
-// Apply only to specific routes
 export const config = {
   matcher: ["/((?!api|_next/static|favicon.ico).*)"] // or  ["/dashboard/:path*", "/admin/:path*"],
 }
