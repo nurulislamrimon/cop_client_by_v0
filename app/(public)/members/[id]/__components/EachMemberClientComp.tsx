aa951612aff801ca979e092bb1d63b7bcf0babbe
nurulislamrimon <nurulislamrimon@gmail.com>
2025-04-25 19:51:21 +0600

    INDIVIDUAL_MEMBER_PREVIEW/DONE

--- app/(public)/members/[id]/__components/EachMemberClientComp.tsx
+++ app/(public)/members/[id]/__components/EachMemberClientComp.tsx
@@ -1,29 +1,28 @@
 import PreviewCard from '@/components/preview-card'
 import React from 'react'
 
-export default function EachMemberClientComp({ member }: { member: Record<string, unknown> }) {
+export default function EachMemberClientComp({ member }: { member?: Record<string, string> }) {
+    let memberData: Record<string, string> = {}
+    let profile_photo = ''
+
+    if (member) {
+        const { profile_photo: photo, ...rest } = member;
+        memberData = rest
+        profile_photo = photo
+        delete rest.profile_photo
+    }
+
 
     return (
         <PreviewCard
-            title={member.full_name as string}
-            data={{
-                name: member.full_name,
-                email: member.email,
-                fatherName: member.father_name,
-                dateOfBirth: member.date_of_birth && new Date(member.date_of_birth as string).toDateString(),
-                phone: member.phone_number,
-                joinDate: member.joining_date && new Date(member.joining_date as string).toDateString(),
-                address: member.address,
-                id: member.id,
-                occupation: member.occupation,
-                referedBy: member.reffered_by,
-            }}
+            title={memberData.full_name as string}
+            data={memberData}
             backLink="/members"
-            imageSrc={`/placeholder.svg?height=128&width=128&text=${(member.name as string)
+            imageSrc={`/placeholder.svg?height=128&width=128&text=${(memberData.name as string)
                 ?.split(" ")
                 ?.map((n) => n[0])
                 ?.join("")}`}
-            badges={[`${member.shares || 0} Shares`, (member?.account_status as string)?.charAt(0).toUpperCase() + (member?.account_status as string)?.slice(1)]}
+            badges={[`${memberData.shares || 0} Shares`, (memberData?.account_status as string)?.charAt(0).toUpperCase() + (memberData?.account_status as string)?.slice(1)]}
         />
     )
 }
